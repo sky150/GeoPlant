86442519d7bb01ac2db54bba3e479c3c33dd76b6
chiara.steindl <chiara.steindl@stud.hslu.ch>
2025-12-02 14:42:37 +0100

    feat: added file for chart prototypes

---
+++ charts.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import folium
+from streamlit_folium import st_folium
+import backend_api  # This imports your logic file
+
+
